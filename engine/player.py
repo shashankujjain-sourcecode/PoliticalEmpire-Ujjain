3f07e0b9d5c26795111fcc5072d3831f7dbdf9fe
shashankujjain-sourcecode <shashank.ujjain@gmail.com>
2026-06-28 10:56:20 +0530

    Update player.py

--- engine/player.py
+++ engine/player.py
@@ -1,1 +1,24 @@
 
+from dataclasses import dataclass
+
+
+@dataclass
+class Player:
+
+    name: str = "Shashank"
+
+    money: int = 5000
+
+    land: float = 0.50
+
+    popularity: int = 5
+
+    trust: int = 10
+
+    supporters: int = 20
+
+    year: int = 2002
+
+    stage: str = "Student Union Election"
+
+    turn: int = 1
