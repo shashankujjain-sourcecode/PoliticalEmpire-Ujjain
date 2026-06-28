ebe26a38f75353ef414977e3510ce422221dfdea
shashankujjain-sourcecode <shashank.ujjain@gmail.com>
2026-06-28 10:56:42 +0530

    Update scenario.py

--- engine/scenario.py
+++ engine/scenario.py
@@ -1,1 +1,14 @@
+import json
 
+
+class ScenarioLoader:
+
+    def __init__(self, path):
+
+        with open(path, encoding="utf-8") as f:
+
+            self.data = json.load(f)
+
+    def get_turn(self, turn):
+
+        return self.data["turns"][turn - 1]
